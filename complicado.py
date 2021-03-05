2a867cc1d8116264d61b87b75848feb2c7d08ebc
bpbb <beatriz.pisoni.barba@gmail.com>
2021-03-05 18:26:39 +0100

    lalaland

---
+++ complicado.py
@@ -0,0 +1,15 @@
+number1 = 20
+number2 = 20
+if number1 > number2:
+    print("number1 is greater than number2")
+elif number1 == number2:
+    print("they are the equal")
+else:
+    print("1<2")
+else:
+    if number1 < number2:
+        print ("1>2")
+    else:
+        print ("equal")
+        print ("because the have the same value")
+
